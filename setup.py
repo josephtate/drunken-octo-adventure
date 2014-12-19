0ee2f2c9a1ba36e97a335303f5d68096936557bb
Joseph Tate <joseph@crunch.io>
2014-12-19 07:46:03 -0500

    Add build num to artifact

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ try:
 except OSError, e:
     print str(e)
 
-buildnum  = os.environ.get('BUILD_NUMBER', 9)
+buildnum  = int(os.environ.get('BUILD_NUMBER', '9'))
 os.system('touch build/drunken-octo-adventure-999-%00d.egg' % buildnum)
